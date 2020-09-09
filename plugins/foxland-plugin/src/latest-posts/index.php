464db8b900f287b23d06e9f721bf0ff15092a5e8
Sami Keijonen <sami.keijonen@foxnet.fi>
2020-09-09 12:20:33 +0300

    Test relative URL.

--- plugins/foxland-plugin/src/latest-posts/index.php
+++ plugins/foxland-plugin/src/latest-posts/index.php
@@ -52,14 +52,14 @@ function render_latest_posts( $attributes ) {
 				if ( 0 === $iterator && has_post_thumbnail() ) :
 					?>
 					<figure class="entry__thumbnail">
-						<a class="entry__thumbnail-link" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
+						<a class="entry__thumbnail-link" href="/<?php echo esc_attr(basename( get_permalink() ) ); ?>" aria-hidden="true" tabindex="-1">
 							<?php echo wp_get_attachment_image( get_post_thumbnail_id( get_the_ID() ), 'post-thumbnail' ); ?>
 						</a>
 					</figure>
 				<?php endif; ?>
 
 				<header class="entry__header">
-					<h2 class="entry__title<?php echo 0 !== $iterator ? ' h3' : ''; ?>"><a class="decoration-none h-decoration-underline color-dark" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<h2 class="entry__title<?php echo 0 !== $iterator ? ' h3' : ''; ?>"><a class="decoration-none h-decoration-underline color-dark" href="/<?php echo esc_attr( basename( get_permalink() ) ); ?>"><?php the_title(); ?></a></h2>
 
 					<div class="entry__byline fw-500">
 						<?php
